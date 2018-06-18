a3c4feafa9b73e1afb5e02be16658c07da5c19d7
raspberrypi3 <raspberrypi3>
2018-06-18 16:19:14 +0900

    add camera tests

---
+++ camera/camera_test2.py
@@ -0,0 +1,21 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 10
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+time.sleep(0.1)
+
+for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
+    t = time.time()
+    image = frame.array
+    image = cv2.flip(image, 1)
+    print('\r', time.time()-t, end='')
+    cv2.imshow("i" ,image)
+    rawCapture.truncate(0)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
